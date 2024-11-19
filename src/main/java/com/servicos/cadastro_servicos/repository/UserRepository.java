a09a44b8b3cd3dadbfe0fb7f91f5ada1dcf4b25d
Weslan alves <weslan27@gmail.com>
2024-11-19 11:40:21 -0300

    Melhorias no design da aba Config e funcionalidade de pesquisa no card de Visitantes
    - Atualizado o design da aba de Configurações para melhorar a experiência do usuário.
    - Adicionada funcionalidade de pesquisa no card de Visitantes, permitindo filtrar visitantes facilmente.
    - Refatoração de estilos e ajustes visuais para consistência no layout.

--- src/main/java/com/servicos/cadastro_servicos/repository/UserRepository.java
+++ src/main/java/com/servicos/cadastro_servicos/repository/UserRepository.java
@@ -5,9 +5,10 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import com.servicos.cadastro_servicos.model.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     

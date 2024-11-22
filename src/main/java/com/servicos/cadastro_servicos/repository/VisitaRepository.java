ae6edb18a15a94ebbd0c7c7a48a8e30e80ed47db
Weslan alves <weslan27@gmail.com>
2024-11-22 09:53:33 -0300

    Melhoria na área de visitante e integração com servidor
    - Adicionada funcionalidade para exclusão de visitantes e visitas na área de visitantes.
    - Melhorado o design dos cards para uma interface mais moderna e intuitiva.
    - Iniciada implementação da lógica para o servidor responder a alterações no banco de dados.
    - Preparação do sistema para sincronização em tempo real.

--- src/main/java/com/servicos/cadastro_servicos/repository/VisitaRepository.java
+++ src/main/java/com/servicos/cadastro_servicos/repository/VisitaRepository.java
@@ -2,17 +2,24 @@ package com.servicos.cadastro_servicos.repository;
 
 
 
-import com.servicos.cadastro_servicos.model.Visita;
-
 import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.stereotype.Repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.servicos.cadastro_servicos.model.Visita;
 
 @Repository
 public interface VisitaRepository extends JpaRepository<Visita, Long> {
 	 List<Visita> findByHorarioSaidaBetween(LocalDateTime startDate, LocalDateTime endDate);
 	  List<Visita> findByHorarioSaidaIsNotNull();
 	  List<Visita> findByVisitanteId(Long visitanteId);
+
+
+	  @Query("SELECT MAX(v.horarioSaida) FROM Visita v")
+	  LocalDateTime findUltimaModificacao();
+
 	  
 }

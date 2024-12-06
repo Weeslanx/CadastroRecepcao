15b2aadcb14e83037a00fde1e643e40c42fa40b8
Weslan alves <weslan27@gmail.com>
2024-12-06 09:10:23 -0300

    atualiz\a;'ao na tabela setor.nome

--- src/main/java/com/servicos/cadastro_servicos/model/Correios.java
+++ src/main/java/com/servicos/cadastro_servicos/model/Correios.java
@@ -1,13 +1,13 @@
 package com.servicos.cadastro_servicos.model;
 
-
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Correios {
@@ -16,19 +16,20 @@ public class Correios {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String funcionario;
-    private String setor;
     private LocalDateTime horario;
-    private String codigoPostal; 
+    private String codigoPostal;
     private String destinatario;
     private String servico;
     private String entregador;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "setor_id")
+    private Setor setor;
 
     public Correios() {}
 
-    
-    public Correios(Long id, String funcionario, String setor, LocalDateTime horario,
-                           String servico, String codigoPostal, String destinatario) {
+    public Correios(Long id, String funcionario, Setor setor, LocalDateTime horario,
+                    String servico, String codigoPostal, String destinatario, String entregador) {
         this.id = id;
         this.funcionario = funcionario;
         this.setor = setor;
@@ -36,41 +37,53 @@ public class Correios {
         this.servico = servico;
         this.codigoPostal = codigoPostal;
         this.destinatario = destinatario;
+        this.entregador = entregador;
     }
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getFuncionario() {
         return funcionario;
     }
+
     public void setFuncionario(String funcionario) {
         this.funcionario = funcionario;
     }
-    public String getSetor() {
+
+    public Setor getSetor() {
         return setor;
     }
-    public void setSetor(String setor) {
+
+    public void setSetor(Setor setor) {
         this.setor = setor;
     }
+
     public LocalDateTime getHorario() {
         return horario;
     }
+
     public void setHorario(LocalDateTime horario) {
         this.horario = horario;
     }
+
     public String getCodigoPostal() {
         return codigoPostal;
     }
+
     public void setCodigoPostal(String codigoPostal) {
         this.codigoPostal = codigoPostal;
     }
+
     public String getDestinatario() {
         return destinatario;
     }
+
     public void setDestinatario(String destinatario) {
         this.destinatario = destinatario;
     }
@@ -83,33 +96,25 @@ public class Correios {
         this.servico = servico;
     }
 
+    public String getEntregador() {
+        return entregador;
+    }
 
-    
+    public void setEntregador(String entregador) {
+        this.entregador = entregador;
+    }
 
     @Override
     public String toString() {
         return "Correios{" +
                 "id=" + id +
                 ", funcionario='" + funcionario + '\'' +
-                ", setor='" + setor + '\'' +
+                ", setor=" + (setor != null ? setor.getNome() : "null") +
                 ", horario=" + horario +
                 ", servico='" + servico + '\'' +
                 ", codigoPostal='" + codigoPostal + '\'' +
                 ", destinatario='" + destinatario + '\'' +
+                ", entregador='" + entregador + '\'' +
                 '}';
     }
-
-
-    public String getEntregador() {
-        return entregador;
-    }
-
-
-    public void setEntregador(String entregador) {
-        this.entregador = entregador;
-    }
-
-    
-
-    
 }
--- src/main/java/com/servicos/cadastro_servicos/repository/CorreiosRepository.java
+++ src/main/java/com/servicos/cadastro_servicos/repository/CorreiosRepository.java
@@ -1,7 +1,20 @@
 package com.servicos.cadastro_servicos.repository;
 
 import com.servicos.cadastro_servicos.model.Correios;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 public interface CorreiosRepository extends JpaRepository<Correios, Long> {
+
+    @Query("SELECT c FROM Correios c JOIN FETCH c.setor WHERE c.id = :id")
+    Correios findByIdWithSetor(@Param("id") Long id);
+
+    @Query("SELECT c FROM Correios c JOIN FETCH c.setor")
+    List<Correios> findAllWithSetor();
 }
+
